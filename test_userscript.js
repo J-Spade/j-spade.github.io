06df09e3b1157e1b8dfc23cc39255f3d94c831f5
J-Spade <j_spade@verizon.net>
2024-01-13 19:27:03 -0500

    narrow down to one set of shake params

--- test_userscript.js
+++ test_userscript.js
@@ -31,27 +31,7 @@ const g_textOptions = [
 
 // CSS defining a "shake" animation
 //   idea shamelessly stolen from https://stackoverflow.com/questions/73537320/
-// TODO: tweak parameters
-const g_shakeAnimation_1 = `
-@keyframes shake {
-    0% { transform: translate(1px, 1px) rotate(0deg); }
-    10% { transform: translate(-1px, -2px) rotate(-1deg); }
-    20% { transform: translate(-3px, 0px) rotate(1deg); }
-    30% { transform: translate(3px, 2px) rotate(0deg); }
-    40% { transform: translate(1px, -1px) rotate(1deg); }
-    50% { transform: translate(-1px, 2px) rotate(-1deg); }
-    60% { transform: translate(-3px, 1px) rotate(0deg); }
-    70% { transform: translate(3px, 1px) rotate(-1deg); }
-    80% { transform: translate(-1px, -1px) rotate(1deg); }
-    90% { transform: translate(1px, 2px) rotate(0deg); }
-    100% { transform: translate(1px, -2px) rotate(-1deg); }
-}
-.shake {
-    animation: shake 0.5s;
-    animation-iteration-count: infinite;
-}
-`
-const g_shakeAnimation_2 = `
+const g_shakeAnimation = `
 @keyframes shake {
     0% { transform: translate(1px, 1px) rotate(0deg); }
     10% { transform: translate(-1px, -3px) rotate(-0.25deg); }
@@ -66,7 +46,7 @@ const g_shakeAnimation_2 = `
     100% { transform: translate(1px, -3px) rotate(-0.25deg); }
 }
 .shake {
-    animation: shake 0.5s;
+    animation: shake 0.25s;
     animation-iteration-count: infinite;
 }
 `
@@ -149,7 +129,7 @@ function getUserBadges() {
 async function doHello(frame, messageContent) {
     // inject shake animation CSS as a <style> tag
     const style = document.createElement("style");
-    style.innerHTML = g_shakeAnimation_2;
+    style.innerHTML = g_shakeAnimation;
     document.head.appendChild(style);
 
     await findPostsByCurrentUser();
